855d0a21d4295d6d920c5f3d4ef3368fc0b7211b
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-20 18:01:47 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -58,7 +58,7 @@ def table_name_for_insert
   end
 
   def self.find_by(attribute)
-    self.column_names.map do |columns|
+    self.column_names.each do |columns|
        DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE '#{columns}' = '#{attribute}'")
       #  DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE '#{columns}' = '#{attribute}'")
   #  end
