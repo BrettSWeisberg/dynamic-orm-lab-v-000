bc2053aecfb2c854ccdbfa35238866ef8148a125
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-20 18:42:46 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,7 +61,7 @@ def table_name_for_insert
     key = attribute.keys[0].to_s
     value = attribute.keys[0]
      DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE '#{key}' = #{value}")
-    
+    binding.pry
   end
 
 end
