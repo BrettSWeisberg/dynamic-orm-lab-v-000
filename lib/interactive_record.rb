980c0e2e3d3059fc82ad448c5f3eb35d8fdd3f43
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-20 17:01:34 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,7 +23,8 @@ class InteractiveRecord
     column_names.compact
   end
 
-  def self.column_names.each do |col_name|
+  def initizalize
+    self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 end
